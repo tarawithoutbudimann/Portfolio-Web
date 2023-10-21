c71910b53198790c7b75ba95af7dc6e522aeef82
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2023-10-22 00:24:29 +0700

    first commit

---
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginRegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest')->except([
+            'logout', 'dashboard'
+        ]);
+    }
+    public function register(){
+        return view('auth.register');
+    }
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|email|max:250|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+        User::create([
+            'name' => $request->name, 
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        Auth::attempt($credentials);
+        $request->session()->regenerate();
+        return redirect()->route('dashboard')
+            ->withSucces('You Have Sucessfully Registered & Logged In !');
+    }
+    public function login(){
+        return view('auth.login');
+    }
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->route('dashboard')
+                -> withSuccess ('You Have Successfully Logged In!');
+        }
+        return back()->withErrors([
+            'email'=> 'Your Provided Credentials Do Not Match With Our Records',
+        ])->onlyInput('email');
+    }
+    public function dashboard(){
+        if (Auth::check()){
+            return view('auth.dashboard');
+        }
+        return redirect()->route('login')
+        ->withErrors([
+            'email'=>'Please Login To Access The Dashboard.',
+        ])->onlyInput('email');
+    }
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+        -> withSuccess('You Have Logged Out Successfully!');;
+    }
+}
+
