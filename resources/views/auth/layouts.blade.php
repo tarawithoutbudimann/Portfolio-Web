85fb0dfc52f9be7be1d6509c05feb4f9a7794a07
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2023-10-24 21:06:16 +0700

    4

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Custom Login Register</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 Custom User Registration & Login Tutorial - AllPHPTricks.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-gray-100">
-    <nav class="bg-white p-4">
+
+    <nav class="bg-white p-2 shadow-md fixed w-full top-0 z-10">
         <div class="container mx-auto flex justify-between items-center">
-            <a href="{{URL('/')}}" class="text-xl font-bold">Tarawithoutbudiman!</a>
-            <button class="block lg:hidden">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
-            </button>
+            <a href="{{ URL('/') }}" class="text-xl font-bold text-gray-800">Tarawithoutbudiman!</a>
             <div class="hidden lg:flex space-x-4">
                 @guest
-                    <a href="{{route('login')}}" class="text-gray-700">Login</a>
-                    <a href="{{route('register')}}" class="text-gray-700">Register</a>
-                @else
-                    <div class="relative">
-                        <span class="block py-2 px-4 text-gray-900">{{ Auth::user()->name }}</span>
-                        <ul class="absolute right-0 z-10 mt-2 space-y-2 text-sm bg-white border rounded-lg shadow-lg">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-                                   class="block px-4 py-2">Logout</a>
+                    <a href="{{ route('login') }}" class="text-gray-700">Login</a>
+                    <a href="{{ route('register') }}" class="text-gray-700">Register</a>
+                    @else    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                            >Logout</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                </form>
                             </li>
                         </ul>
                     </div>
@@ -36,9 +39,16 @@
             </div>
         </div>
     </nav>
-    <div class="container mx-auto mt-4 p-4">
-        @yield('content')
+
+    <div class="pt-16">
+        <div class="container mx-auto mt-4 p-4">
+            @yield('content')
+        </div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
