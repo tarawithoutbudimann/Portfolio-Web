76966a6913538524fa2f0373949f3313acaca204
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2023-12-05 15:41:19 +0700

    6th

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -8,7 +8,6 @@
     <title>Laravel 10 Custom User Registration & Login Tutorial - AllPHPTricks.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
-    @vite(['resources/css/app.css','resources/js/app.js'])
     <link rel="stylesheet" href="{{asset('build\assets\app-a1835b0b.css')}}">
 </head>
 
