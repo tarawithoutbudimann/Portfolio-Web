a36cdd396a5a05e58a948fc3b8ecec31d31e8a0f
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2023-12-05 15:22:00 +0700

    5th

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('build\assets\app-a1835b0b.css')}}">
 </head>
 
 <body class="bg-gray-100">
@@ -50,6 +51,7 @@
         </div>
     </div>
 
+    <script src="{{asset('build\assets\app-55b385e2.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous">
